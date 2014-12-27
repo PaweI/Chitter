6427c37205df7d84fc65aef6a311ed495d50795a
Pavel Redics <pavel.redics@gmail.com>
2014-12-27 00:03:35 +0000

    create user in test to let lezave peep

--- features/step_definitions/peep_steps.rb
+++ features/step_definitions/peep_steps.rb
@@ -1,8 +1,20 @@
+def create_user
+  visit '/'
+  click_on "Sign up"
+  fill_in 'name', with: "Samuel Russell Hampden Joseph"
+  fill_in 'username', with: "tansaku"
+  fill_in 'email', with: "sam@makersacademy.com"
+  fill_in 'password', with: "testtest"
+  fill_in 'password_confirmation', with: "testtest"
+  click_on "Sign up"
+end
+
 When(/^open main page$/) do
   visit '/'
 end
 
 When(/^fill peep box and click PEEP$/) do
+  create_user
   fill_in 'peep', with: "Dream on, dream on.."
   click_on 'PEEP'
 end
