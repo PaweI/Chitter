fc3ba6136cdc153429d5b7f354ad45454526a568
Pavel Redics <pavel.redics@gmail.com>
2014-12-28 12:41:55 +0000

    refactored index.haml, created helper method time_word

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,10 @@
 def current_user
   @current_user ||= User.get(session[:user_id]) if session[:user_id]
 end
+
+def time_word(peep)
+  return "#{(Time.now - peep.time).to_i} seconds ago" if (0..59).include?(Time.now - peep.time)
+  return "#{(Time.now - peep.time).to_i / 60} minutes ago" if (60..3599).include?(Time.now - peep.time)
+  return "#{(Time.now - peep.time).to_i / 3600} hours ago" if (3600..86400).include?(Time.now - peep.time)
+  "#{(Time.now - peep.time).to_i / 86400} days ago"
+end
